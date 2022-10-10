4a685197f44fb5487df089d7c1273b0ad7e14901
doinel1a <doinel1atanasiu@gmail.com>
2022-10-10 21:22:27 +0200

    Footer component implemented

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import MetaHead from '../components/MetaHead';
 import Header from '../components/Header';
 import LinksList from '../components/LinksList';
+import Footer from '../components/Footer';
 
 export default function Home() {
 	return (
@@ -10,6 +11,7 @@ export default function Home() {
 			<main className='w-full max-w-2xl min-h-screen flex flex-col justify-center items-center mx-auto p-8'>
 				<Header />
 				<LinksList />
+				<Footer />
 			</main>
 		</>
 	);
