f27bdfd2f15def30659d71131496a0494cebf92e
doinel1a <doinel1atanasiu@gmail.com>
2022-10-10 21:06:02 +0200

    Global colors css variables configured

---
+++ src/config/colors.ts
@@ -0,0 +1,33 @@
+const colors = [
+	// Light theme
+	{
+		property: '--primary-light', // Background color
+		value: '#f5f5f5', // neutral-100
+	},
+	{
+		property: '--color-light', // Text color
+		value: '#303030',
+	},
+
+	// Dark theme
+	{
+		property: '--primary-dark', // Background color
+		value: '#171717', // neutral-900
+	},
+	{
+		property: '--color-dark', // Text color
+		value: '#f3f3f3',
+	},
+
+	// Accent
+	{
+		property: '--accent-primary',
+		value: '#2563eb', // blue-600
+	},
+	{
+		property: '--accent-primary-state',
+		value: '#1e40af', // blue-800
+	},
+];
+
+export default colors;
